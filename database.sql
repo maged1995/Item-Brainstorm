6913ac1683b5c5b6b3a0e204b4171069754878e1
Maged A. Saad <magedsaadaziz@hotmail.com>
2018-07-27 21:53:47 +0200

    corrections & added new table 'genres'

--- database.sql
+++ database.sql
@@ -1,19 +1,27 @@
 create database Item_Brainstorm;
 
-create table Users(
+create table users(
   id int not null auto_increment, email varchar(254),
   username varchar(24), passwordHash varchar(32),
   birthDate date,
   constraint chk_email check (email like '%_@__%.__%'),
-  primary key(id));
+  primary key(id)
+);
 
-create table Items(
+create table items(
   id int not null auto_increment,
   name varchar(24) not null, imageFol text not null,
-  applied boolean, dateCreated date);
+  applied boolean, dateCreated date
+);
 
-create table type(
-  id int not null auto_increment, name varchar(30));
+create table types(
+  id int not null auto_increment, name varchar(30)
+);
 
-create table Users_Items(
-  type int not null, item int not null);
+create table genres(
+  id int not null auto_increment, name varchar(30)
+);
+
+create table users_items(
+  type int not null, item int not null
+);
