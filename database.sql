e405c00c242057154e328ef4ebe1d5c83ee6c913
Maged A. Saad <magedsaadaziz@hotmail.com>
2018-07-27 21:43:06 +0200

    added new table 'type'

--- database.sql
+++ database.sql
@@ -1,10 +1,16 @@
 create database Item_Brainstorm;
 
 create table Users(
-  id int NOT NULL AUTO_INCREMENT, email varchar(254),
+  id int not null auto_increment, email varchar(254),
   username varchar(24), passwordHash varchar(32),
-  date birthday,
+  birthDate date,
   constraint chk_email check (email like '%_@__%.__%'),
   primary key(id));
 
-create table Items(id int NOT NULL AUTO_INCREMENT, name varchar(24) not null, imageFol text);
+create table Items(
+  id int not null auto_increment,
+  name varchar(24) not null, imageFol text not null,
+  dateCreated date);
+
+create table type(
+  id int not null auto_increment, name varchar(30));
