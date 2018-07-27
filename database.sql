9838afe4fa2a6efbcf9082598cb380322a9c5256
Maged A. Saad <magedsaadaziz@hotmail.com>
2018-07-27 21:38:35 +0200

    added attributes to Users and Items

--- database.sql
+++ database.sql
@@ -3,6 +3,7 @@ create database Item_Brainstorm;
 create table Users(
   id int NOT NULL AUTO_INCREMENT, email varchar(254),
   username varchar(24), passwordHash varchar(32),
+  date birthday,
   constraint chk_email check (email like '%_@__%.__%'),
   primary key(id));
 
