242c5647f613f8943d470ca237a13ba401261b25
Maged A. Saad <magedsaadaziz@hotmail.com>
2018-07-30 07:03:48 +0200

    Added Google login API to 'Login.php'

--- Login.php
+++ Login.php
@@ -8,6 +8,41 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="main.css">
     <title>Sign in</title>
+    <script src="https://www.gstatic.com/firebasejs/5.3.0/firebase.js"></script>
+    <script>
+      // Initialize Firebase
+      // TODO: Replace with your project's customized code snippet
+      var config = {
+        apiKey: "AIzaSyAKmzy8uOasVhEtTlCDaRoa7G18nF98CTw",
+        authDomain: "item-brainstorm.firebaseapp.com",
+        databaseURL: "https://item-brainstorm.firebaseio.com",
+        projectId: "item-brainstorm",
+        storageBucket: "item-brainstorm.appspot.com",
+        messagingSenderId: "334249970569"
+      };
+      firebase.initializeApp(config);
+    </script>
+    <script>
+      function callGoogleSignIn(){
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+      }
+    </script>
   </head>
   <body>
     <?php include "header.html" ?>
@@ -19,6 +54,7 @@
           <label for="password">Password: </label><input type="password" placeholder="Username.." name="password"/><br/>
           <input type="submit" value="login"/>
         </form>
+        <input type="button" onclick=callGoogleSignIn() value="Google Sign In"/>
         <!-- google, facebook, and other login api's will be here -->
       </div>
     </main>
